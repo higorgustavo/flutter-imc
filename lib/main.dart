0dd871fa716d0f9bcab0f744bfebf030de504bcf
higorgustavo10 <higor-gustavo_lp@hotmail.com>
2020-04-15 02:19:23 -0300

    Título

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'IMC'),
+      home: MyHomePage(title: 'Flutter IMC'),
     );
   }
 }
