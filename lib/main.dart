f0230b45c9f05ba9a1e82766221b77b2e12f8458
higorgustavo10 <higor-gustavo_lp@hotmail.com>
2020-04-15 02:23:22 -0300

    Iniciando Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter IMC'),
+      home: MyHomePage(title: 'IMC'),
     );
   }
 }
